671ed250f1e34caaa6fe64c758ae159cd5c9d388
Sorena <ypiran2017@gmail.com>
2024-02-26 21:06:25 +0330

    add Home page to app and setting up finished

---
+++ Home/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+# class TodoCreatedForm(forms.ModelForm):
+#     class Meta:
+#         model = Todo
+#         fields = ('title', 'body', 'created')
--- Home/views.py
+++ Home/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from django.views import View, generic
 
 # Create your views here.
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'home.html')
+        pass
+    def post(self,request):
+        pass
