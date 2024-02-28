11d77033ff2791f27de811d83abe6369d22275f2
Sorena <ypiran2017@gmail.com>
2024-02-28 13:49:50 +0330

    adding edit function for user post

--- Home/urls.py
+++ Home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import HomeView, NewPostView, PostDetailView,EditPostView
+from .views import *
 
 app_name = 'Home'
 urlpatterns = [
     path('', HomeView.as_view(), name='HomeURL'),
     path('newpost/', NewPostView.as_view(), name='NewPostURL'),
-    path('post/<int:post_id>/<slug:post_slug>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:post_id>/', PostDetailView.as_view(), name='post_detail'),
     path('Edit/<int:post_id>', EditPostView.as_view(), name='EditPost'),
+    path('Delete/<int:post_id>', DeletePostView.as_view(), name='DeletePost'),
 ]
--- Home/views.py
+++ Home/views.py
@@ -15,8 +15,8 @@ class HomeView(View):
         return render(request, 'Home/post.html', {'posts': posts})
 
 class PostDetailView(View):
-    def get(self, request, post_id, post_slug):
-        publishedpost = Post.objects.get(id=post_id, slug=post_slug)
+    def get(self, request, post_id):
+        publishedpost = Post.objects.get(id=post_id)
         return render(request, 'Home/detailpost.html', {'posts': publishedpost})
 
 class NewPostView(LoginRequiredMixin, View):
@@ -31,7 +31,7 @@ class NewPostView(LoginRequiredMixin, View):
             newpostdata.slug = slugify(rform.cleaned_data['body'][:30])
             newpostdata.save()
             messages.error(request, 'Post Was Published :)', extra_tags='success')
-            return redirect('Home:post_detail', newpostdata.id, newpostdata.slug)
+            return redirect('Home:post_detail', newpostdata.id)
         else:
             messages.error(request, 'Bad Parameters', extra_tags='danger')
             return render(request, 'Home/Newpost.html', {'form': self.form})
@@ -40,7 +40,19 @@ class EditPostView(View):
     def get(self, request, post_id):
         if request.user.is_authenticated:
             post = Post.objects.get(id=post_id)
-            return render(request, 'Home/update.html', {'form':''})
+            form = NewPostForm(instance=post)
+            return render(request, 'Home/update.html', {'form': form, 'post_id':post_id})
         else:
             messages.error(request, 'You should Login to App', extra_tags='danger')
             return redirect('accounts:LoginURL')
+    def post(self, request, post_id):
+        post = Post.objects.get(id=post_id)
+        recive_form = NewPostForm(request.POST, instance=post)
+        if recive_form.is_valid():
+            recive_form.save()
+            messages.success(request, "Post Edit Successfully", extra_tags="success")
+            return redirect('Home:post_detail', post_id)
+
+class DeletePostView(View):
+    def get(self, request, post_id):
+        pass
